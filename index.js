e0df728fbc115608dcbe847a89931b8f46099499
tsengjonathan <tseng.j@husky.neu.edu>
2019-12-01 16:36:13 -0500

    Replace asychronous fs function calls with synchronous ones

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const cp = require('child_process');
-const fs = require('fs');
+import cp from 'child_process';
+import fs from 'fs';
 
 const args = process.argv.slice(2);
 
 if (args.length > 0) {
   cp.fork(args[0]);
 } else {
-  fs.readdir('src/', (err, files) => {
-    cp.fork(`src/${files.sort().pop()}`);
-  });
+  const files = fs.readdirSync('src/');
+  cp.fork(`src/${files.sort().pop()}`);
 }
