30efd98aa7d0ba9f2fdfd77f349c3db648283d44
tsengjonathan <tseng.j@husky.neu.edu>
2019-12-07 12:43:08 -0500

    Parse only .js files when reading directory

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const args = process.argv.slice(2);
 if (args.length > 0) {
   cp.fork(args[0]);
 } else {
-  const files = fs.readdirSync('src/');
+  const files = fs.readdirSync('src/').filter(file => file.endsWith('.js'));
   cp.fork(`src/${files.sort().pop()}`);
 }
