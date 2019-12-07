c1fe90d7a984ca5f3eea41f61cf4013d97cb5ab9
tsengjonathan <tseng.j@husky.neu.edu>
2019-12-07 12:05:27 -0500

    Refine 05.js
    The incode computer from 05.js is needed for 07.js. Code in 05.js is now
    exported by default to be used by other challenges.

--- src/05.js
+++ src/05.js
@@ -4,44 +4,52 @@ const instructions = fs.readFileSync('data/05.txt', 'utf8').split(',').map(str =
 
 const jumpIndexes = [0, 4, 4, 2, 2, 3, 3, 4, 4];
 
-const input = 5;
-let idx = 0;
+function runIncodeComputer(instArgs, inputs) {
+  const instructions = instArgs.slice();
+  let idx = 0;
+  while (idx < instructions.length) {
+    const instruction = instructions[idx].toString().padStart(5, '0');
+    const opcode = parseInt(instruction.substring(instruction.length - 2));
+    const [modeB, modeA] = instruction.substring(1, 3).split('').map(str => parseInt(str));
+    const [argA, argB, argC] = instructions.slice(idx + 1, idx + 4);
+    const numA = modeA === 0 ? instructions[argA] : argA;
+    const numB = modeB === 0 ? instructions[argB] : argB;
 
-while (idx < instructions.length) {
-  const instruction = instructions[idx].toString().padStart(5, '0');
-  const opcode = parseInt(instruction.substring(instruction.length - 2));
-  const [modeB, modeA] = instruction.substring(1, 3).split('').map(str => parseInt(str));
-  const [argA, argB, argC] = instructions.slice(idx + 1, idx + 4);
-  const numA = modeA === 0 ? instructions[argA] : argA;
-  const numB = modeB === 0 ? instructions[argB] : argB;
+    if (opcode === 1) {
+      instructions[argC] = numA + numB;
+    } else if (opcode === 2) {
+      instructions[argC] = numA * numB;
+    } else if (opcode === 3) {
+      instructions[argA] = inputs.shift();
+    } else if (opcode === 4) {
+      console.log(numA);
+    } else if (opcode === 5) {
+      if (numA !== 0) {
+        idx = numB;
+        continue;
+      }
+    } else if (opcode === 6) {
+      if (numA === 0) {
+        idx = numB;
+        continue
+      }
+    } else if (opcode === 7) {
+      instructions[argC] = numA < numB ? 1 : 0;
+    } else if (opcode === 8) {
+      instructions[argC] = numA === numB ? 1 : 0;
+    } else if (opcode === 99) {
+      break;
+    } else {
+      throw new Error(`Unrecognized opcode ${opcode}`);
+    }
 
-  if (opcode === 1) {
-    instructions[argC] = numA + numB;
-  } else if (opcode === 2) {
-    instructions[argC] = numA * numB;
-  } else if (opcode === 3) {
-    instructions[argA] = input;
-  } else if (opcode === 4) {
-    console.log(numA);
-  } else if (opcode === 5) {
-    if (numA !== 0) {
-      idx = numB;
-      continue;
-    }
-  } else if (opcode === 6) {
-    if (numA === 0) {
-      idx = numB;
-      continue
-    }
-  } else if (opcode === 7) {
-    instructions[argC] = numA < numB ? 1 : 0;
-  } else if (opcode === 8) {
-    instructions[argC] = numA === numB ? 1 : 0;
-  } else if (opcode === 99) {
-    break;
-  } else {
-    throw new Error(`Unrecognized opcode ${opcode}`);
+    idx += jumpIndexes[opcode];
   }
-
-  idx += jumpIndexes[opcode];
 }
+
+console.log('Part 1:');
+runIncodeComputer(instructions, [1]);
+console.log('Part 2:');
+runIncodeComputer(instructions, [5]);
+
+export default runIncodeComputer;
