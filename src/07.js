c751e8ee1cd7e60c488b78f190daa63285a19915
tsengjonathan <tseng.j@husky.neu.edu>
2019-12-08 12:35:23 -0500

    Complete 07.js

---
+++ src/07.js
@@ -0,0 +1,65 @@
+import fs from 'fs';
+import _ from 'lodash';
+
+import runIncodeComputer from './utils/runIncodeComputer';
+
+const instructions = fs.readFileSync('data/07.txt', 'utf8').split(',').map(str => parseInt(str));
+
+function generatePermutations(input) {
+  const result = [];
+  function permute(arr, m = []) {
+    if (arr.length === 0) {
+      result.push(m);
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next));
+      }
+    }
+  }
+  permute(input);
+  return result;
+}
+
+let maxThrusterSignal = 0;
+
+const permutations = generatePermutations([0, 1, 2, 3, 4]);
+permutations.forEach(permutation => {
+  let signal = 0;
+  permutation.forEach(phaseSetting => {
+    signal = runIncodeComputer(instructions, [phaseSetting, signal]).output;
+  });
+  maxThrusterSignal = Math.max(maxThrusterSignal, signal);
+});
+
+console.log(`Part 1: ${maxThrusterSignal}`);
+
+const feedbackLoopPermutations = generatePermutations([5, 6, 7, 8, 9]);
+let maxThrusterOutput = 0;
+
+feedbackLoopPermutations.forEach(permutation => {
+  let thrusterOutput = 0;
+  const ampIndexes = [0, 0, 0, 0, 0];
+  let ampIdx = 0;
+  const settings = permutation.map((phaseSetting, idx) => idx === 0 ? [phaseSetting, 0] : [phaseSetting]);
+  const ampInstructions = _.fill(Array(5), instructions);
+  while (true) {
+    const program = runIncodeComputer(ampInstructions[ampIdx], settings[ampIdx], ampIndexes[ampIdx]);
+    const nextIdx = (ampIdx + 1) % 5;
+    
+    ampIndexes[ampIdx] = program.index;
+    settings[nextIdx].push(program.output);
+    ampInstructions[ampIdx] = program.instructions;
+    if (ampIdx === 4) {
+      thrusterOutput = Math.max(thrusterOutput, program.output || 0);
+      if (program.reason === 99) {
+        break;
+      }
+    }
+    ampIdx = nextIdx;
+  };
+  maxThrusterOutput = Math.max(maxThrusterOutput, thrusterOutput);
+})
+
+console.log(`Part 2: ${maxThrusterOutput}`);
